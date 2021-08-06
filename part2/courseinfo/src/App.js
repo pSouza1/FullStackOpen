2183eccfac97a70cc7a1c8b847f5bb0bd702ba82
Pedro Sousa <pedrosousa93@gmail.com>
2021-08-06 23:11:09 +0100

    Part2 - Exercise2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>    
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+    </div>
+  )
+}
+
+const Course = (props) => {
+
+  return(
+     <div>
+      <Header course={props.course} />
+      <Content course={props.course} />
+      {/*<Total course={props.course} />*/}
+    </div>
+  )
+}
+
+
+{/*
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+*/}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+
+export default App;
