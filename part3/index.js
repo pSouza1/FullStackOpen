cf9f8827d0ecd6d1afea5fa0296d635290abea3d
Pedro Sousa <pedrosousa93@gmail.com>
2021-09-07 22:40:34 +0100

    Part3 - Exercise3.2

--- part3/index.js
+++ part3/index.js
@@ -29,6 +29,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+  app.get('/info', (request, response) => {
+
+    var dt = new Date();
+    response.send('<p>Phonebook has info for ' +persons.length+ ' people'+'<p>' +dt)
+  })
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
