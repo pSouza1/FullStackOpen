89ddbcfd6b22ceb8c54252a207bb0ad87d591e46
Pedro Sousa <pedrosousa93@gmail.com>
2021-09-08 00:20:43 +0100

    Part3 - Exercise3.6

--- part3/index.js
+++ part3/index.js
@@ -65,9 +65,15 @@ const generateId = () => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if (!body.name) {
+  if (!body.name || !body.number) {
     return response.status(400).json({ 
-      error: 'name missing' 
+      error: 'name or number missing' 
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(406).json({ 
+    error: 'name must be unique' 
     })
   }
 
