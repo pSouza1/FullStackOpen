e383e5fb07d521efff74c6888bada47be7aa9a74
Pedro Sousa <pedrosousa93@gmail.com>
2021-09-13 22:47:50 +0100

    Part3 - Exercise3.9

--- part3/index.js
+++ part3/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require('morgan')
-const cors = require('cors')
 
 morgan.token('bodyContent', function (req, res) { return req.body })
 
 app.use(express.json())
 app.use(morgan('tiny'))
-app.use(cors())  
+app.use(express.static('build'))
 
 
 let persons = [
