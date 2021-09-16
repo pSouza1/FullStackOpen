3ac85374b28b17be5ff1e9bc1172c23be9ff595a
Pedro Sousa <pedrosousa93@gmail.com>
2021-09-17 00:00:26 +0100

    Part3 - Exercise3.17

--- part3/index.js
+++ part3/index.js
@@ -84,6 +84,22 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
