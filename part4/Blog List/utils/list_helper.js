a02c676819620777ebe1ab4e0f8c5a78414474ae
Pedro Sousa <pedrosousa93@gmail.com>
2021-10-12 22:27:54 +0100

    Part4 - Exercise4.5

--- part4/Blog List/utils/list_helper.js
+++ part4/Blog List/utils/list_helper.js
@@ -12,10 +12,24 @@ const totalLikes = (blogs) =>{
     : blogs.reduce(reducer, 0)
 }  
   
+const mostLikes = (blogs) =>{
+  if (blogs.length===0){return{}} else {
+  
+  let topLikes = blogs[0].likes
+  let topIndex = 0
 
-
+  for (let index = 0; index < blogs.length; index++) {
+    if (blogs[index].likes > topLikes){
+      topLikes = blogs[index].likes
+      topIndex = index
+    }
+  }
+  return blogs[topIndex].likes
+  }
+}
 
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostLikes
   }
