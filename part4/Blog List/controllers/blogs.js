5d0f2a8f78d175ff9c3479cca32e59461923409c
Pedro Sousa <pedrosousa93@gmail.com>
2021-10-15 23:02:43 +0100

    Part4 - Exercise4.13

--- part4/Blog List/controllers/blogs.js
+++ part4/Blog List/controllers/blogs.js
@@ -20,15 +20,42 @@ blogsRouter.post("/", (request, response) => {
 if ((!body.title) || (!body.url)) {
   response.status(400).end()
 }else{
-
-
-    const blog = new Blog(body);
+  const blog = new Blog(body);
 
   blog.save().then((result) => {
     response.status(201).json(result);
   })
 }
 });
+
+
+blogRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  if (!body.likes) {
+      body.likes = 0
+  }
+
+  const blog = new Blog(body);
+
+  try {
+      const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+      logger.info(`blog ${blog.title} updated`)
+      response.json(updatedBlog.toJSON())
+  } catch (exception) {
+      next(exception)
+  }
+})
+
 
 
 module.exports = blogsRouter;
