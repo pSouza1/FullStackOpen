97487db14957ed4ecc77dbd091d7dd1d90fc6eb6
Pedro Sousa <pedrosousa93@gmail.com>
2021-10-19 23:46:33 +0100

    Part4 - Exercise4.18

--- part4/Blog List/app.js
+++ part4/Blog List/app.js
@@ -9,6 +9,7 @@ const mongoose = require("mongoose");
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
 
+const loginRouter = require('./controllers/login')
 
 
 logger.info("connecting to", config.MONGODB_URI);
@@ -31,5 +32,6 @@ app.use(express.json());
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app;
