894a4afa446e9d31eae4264387b4f988c27c73e9
Pedro Sousa <pedrosousa93@gmail.com>
2021-11-04 22:42:06 +0000

    Part5 - Exercise5.15

--- part5/bloglist/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist/bloglist-frontend/src/components/Blog.test.js
@@ -38,3 +38,20 @@ test('view button, and can see blog detail', () => {
   expect(showAll).toHaveTextContent('https://jestjs.io/blog/2017/01/30/a-great-developer-experience')
   expect(showAll).toHaveTextContent('21')
 })
+
+test('if the like button is clicked twice, the event handler the component received as props is called twice', () => {
+  const component = render(
+    <Blog blog={blog} mockHandler />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  const showAll = component.container.querySelector('.showAll')
+
+  const likeButton = component.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(showAll).toHaveTextContent('23')
+})
