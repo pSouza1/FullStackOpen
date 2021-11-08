e67259041b9e9575b013e82478f15e81dc164283
Pedro Sousa <pedrosousa93@gmail.com>
2021-11-08 23:26:48 +0000

    Part5 - Exercise5.18

--- part5/bloglist/bloglist-frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist/bloglist-frontend/cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,15 @@
 describe('Blog app', function () {
   beforeEach(function () {
+
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    cy.visit('http://localhost:3000')
+
+    const user = {
+      'username': 'userTest',
+      'password': 'passTest',
+      'name': 'nameTest'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
   it('Login form is shown', function () {
@@ -8,5 +17,23 @@ describe('Blog app', function () {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+})
+
+
+describe('Login', function () {
+  it('succeeds with correct credentials', function () {
+    cy.get('input:first').type('userTest')
+    cy.get('input:last').type('passTest')
+    cy.contains('login').click()
+
+    cy.contains('logged in')
+    cy.contains('logout').click()
+  })
+
+  it('fails with wrong credentials', function () {
+    cy.get('input:first').type('user123')
+    cy.get('input:last').type('pass123')
+    cy.contains('login').click()
   })
 })
