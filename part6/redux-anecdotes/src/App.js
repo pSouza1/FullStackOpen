9d781e6432e1548a67e87dae67a92a73d291bfb5
Pedro Sousa <pedrosousa93@gmail.com>
2021-11-15 23:55:26 +0000

    Part6 - Exercise6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -5,6 +5,12 @@ import {asObject} from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+
+  const sortedAnecdotes = [...anecdotes].sort((a, b) => {
+    return b.votes - a.votes
+  })
+
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -21,7 +27,8 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+
 import App from './App'
 import reducer from './reducers/anecdoteReducer'
 
