8bab1fee458bdaf51d76c6650654ba0d890bb6fe
Pedro Sousa <pedrosousa93@gmail.com>
2021-11-24 22:47:48 +0000

    Part7 - Exercise7.4

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React , { useState } from 'react'
 import { Switch, Route, Link,  BrowserRouter as Router, useParams, useHistory } from "react-router-dom"
+
+import { useField } from './hooks'
 
 
 const Menu = () => {
@@ -47,9 +49,10 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
   const history = useHistory()
   const [notification, setNotification] = useState('')
 
@@ -76,15 +79,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
